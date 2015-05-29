de01c1ad4385e4113159d4e401c1760d5fb3b8da
KingLeonidas90 <marlon.mattern5@googlemail.com>
2015-05-29 12:15:23 +0200

    Appointment vollständig

--- Ue1-AdressMap/src/AdressBook/Appointment.java
+++ Ue1-AdressMap/src/AdressBook/Appointment.java
@@ -4,37 +4,62 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Appointment {
-	
-private StringProperty terminKategorie = new SimpleStringProperty();
-public String getTerminkategorie(){
-	return terminKategorie.get();
-	
-}
-public void setTerminkategorie(String terminKategorie){
-	this.terminKategorie.set(terminKategorie);
-}
-private StringProperty datum = new SimpleStringProperty();
-	public String getDatum(){
-		return datum.get();
-	
+
+	private StringProperty terminKategorie = new SimpleStringProperty();
+
+	public String getTerminkategorie() {
+		return terminKategorie.get();
+
 	}
-public void setDatum(String datum){
-	this.datum.set(datum);
-}
-public StringProperty startUhrzeit = new SimpleStringProperty();
-	public String getStartUhrzeit(){
+
+	public void setTerminkategorie(String terminKategorie) {
+		this.terminKategorie.set(terminKategorie);
+	}
+
+	private StringProperty datum = new SimpleStringProperty();
+
+	public String getDatum() {
+		return datum.get();
+
+	}
+
+	public void setDatum(String datum) {
+		this.datum.set(datum);
+	}
+
+	private StringProperty startUhrzeit = new SimpleStringProperty();
+
+	public String getStartUhrzeit() {
 		return startUhrzeit.get();
 	}
-public void setStartUhrzeit(String startUhrzeit){
-	this.startUhrzeit.set(startUhrzeit);
-}
-private StringProperty endUhrzeit = new SimpleStringProperty();
-public String getEndUhrzeit(){
-	return endUhrzeit.get();
-}
-public void setEndUhrzeit(String endUhrzeit){
-	this.endUhrzeit.set(endUhrzeit);
-}
 
+	public void setStartUhrzeit(String startUhrzeit) {
+		this.startUhrzeit.set(startUhrzeit);
+	}
+
+	private StringProperty endUhrzeit = new SimpleStringProperty();
+
+	public String getEndUhrzeit() {
+		return endUhrzeit.get();
+	}
+
+	public void setEndUhrzeit(String endUhrzeit) {
+		this.endUhrzeit.set(endUhrzeit);
+	}
+	private StringProperty terminBezeichnung = new SimpleStringProperty();
+	public String getTerminBezeichnung(){
+		return terminBezeichnung.get();
+		
+	}
+	public void setTerminBezeichnung(String terminBezeichnung){
+		this.setTerminBezeichnung(terminBezeichnung);
+	}
+	private StringProperty terminBeschreibung = new SimpleStringProperty();
+	public String getTerminBeschreibung(){
+		return terminBeschreibung.get();
+	}
+	public void setTerminBeschreibung(String terminBeschreibung){
+		this.terminBeschreibung.set(terminBeschreibung);
+	}
 
 }
