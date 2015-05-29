3587699ca6b65df8825c3aadd48396041f4025dd
KingLeonidas90 <marlon.mattern5@googlemail.com>
2015-05-29 14:23:27 +0200

    1b

--- Ue1-AdressMap/src/calendar/Appointment.java
+++ Ue1-AdressMap/src/calendar/Appointment.java
@@ -3,6 +3,7 @@ package calendar;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Year;
 
 import javafx.beans.property.ObjectProperty;
@@ -29,54 +30,44 @@ public class Appointment {
 	public LocalDate getDatum() {return datum.get();}
 	public void setDatum(LocalDate date) {	this.datum.set(date);	}
 
-	
-	private StringProperty startUhrzeit = new SimpleStringProperty();
+	//### STARTUHRZEIT ###///
+	private ObjectProperty<LocalTime> startUhrzeit = new SimpleObjectProperty<LocalTime>();
+	public LocalTime getStartUhrzeit() {return startUhrzeit.get();}
+	public void setStartUhrzeit(LocalTime startTime) {this.startUhrzeit.set(startTime);}
 
-	public String getStartUhrzeit() {
-		return startUhrzeit.get();
-	}
+	/// ### ENDUHRZEIT ###///
+	private ObjectProperty<LocalTime> endUhrzeit = new SimpleObjectProperty<LocalTime>();
+	public LocalTime getEndUhrzeit() {return endUhrzeit.get();}
+	public void setEndUhrzeit(LocalTime endTime) throws BeforeException {
+		if(endTime.isBefore(getStartUhrzeit())) throw new BeforeException();
+		this.endUhrzeit.set(endTime);}
 
-	public void setStartUhrzeit(String startUhrzeit) {
-		this.startUhrzeit.set(startUhrzeit);
-	}
-
-	private StringProperty endUhrzeit = new SimpleStringProperty();
-
-	public String getEndUhrzeit() {
-		return endUhrzeit.get();
-	}
-
-	public void setEndUhrzeit(String endUhrzeit) {
-		this.endUhrzeit.set(endUhrzeit);
-	}
+	/// ### TERMINBEZEICHNUNG ###///
 	private StringProperty terminBezeichnung = new SimpleStringProperty();
-	public String getTerminBezeichnung(){
-		return terminBezeichnung.get();
-		
-	}
-	public void setTerminBezeichnung(String terminBezeichnung){
-		this.terminBezeichnung.set(terminBezeichnung);
-	}
+	public String getTerminBezeichnung(){return terminBezeichnung.get();}
+	public void setTerminBezeichnung(String terminBezeichnung){this.terminBezeichnung.set(terminBezeichnung);}
 	private StringProperty terminBeschreibung = new SimpleStringProperty();
-	public String getTerminBeschreibung(){
-		return terminBeschreibung.get();
-	}
-	public void setTerminBeschreibung(String terminBeschreibung){
-		this.terminBeschreibung.set(terminBeschreibung);
-	}
-
 	
+	/// ### TERMINBESCHREIBUNG ###///
+	public String getTerminBeschreibung(){return terminBeschreibung.get();}
+	public void setTerminBeschreibung(String terminBeschreibung){this.terminBeschreibung.set(terminBeschreibung);}
+
+	/// ### MAIN METHODE ###///
 	public static void main(String[] args) {
 		Appointment termin = new Appointment();
 		termin.setTerminBezeichnung("Bez-ONE");
-		termin.setStartUhrzeit("19:30");
-		termin.setEndUhrzeit("18:00");
+		termin.setStartUhrzeit(LocalTime.of(12, 20));
+		try {
+			termin.setEndUhrzeit(LocalTime.of(11, 20));
+		} catch (BeforeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		termin.setDatum(LocalDate.of(2015, 05, 29));
 		System.out.println(termin.getStartUhrzeit());
 		System.out.println(termin.getEndUhrzeit());
 		System.out.println(termin.getTerminBezeichnung());
 		System.out.println(termin.getDatum());
-		
 	}
 	
 }
